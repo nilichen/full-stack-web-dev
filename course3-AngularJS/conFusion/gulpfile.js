0dee83c78a9b3bb2039e45fabb0cdc9ba73bbb95
nilichen <lichenni1990@gmail.com>
2017-04-10 13:52:15 -0700

    angular scope

--- course3-AngularJS/conFusion/gulpfile.js
+++ course3-AngularJS/conFusion/gulpfile.js
@@ -13,6 +13,7 @@ var gulp = require('gulp'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
     del = require('del');
+var ngannotate = require('gulp-ng-annotate');    
 
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
@@ -32,11 +33,12 @@ gulp.task('default', ['clean'], function() {
 
 gulp.task('usemin',['jshint'], function () {
   return gulp.src('./app/menu.html')
-      .pipe(usemin({
-        css:[minifycss(),rev()],
-        js: [uglify(),rev()]
-      }))
-      .pipe(gulp.dest('dist/'));
+    .pipe(usemin({
+      css:[minifycss(),rev()],
+      js: [ngannotate(),uglify(),rev()]
+    }))
+    
+    .pipe(gulp.dest('dist/'));
 });
 
 // Images
