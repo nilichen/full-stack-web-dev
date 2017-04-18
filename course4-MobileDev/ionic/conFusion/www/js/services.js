4f69d538abbb6477e2e25eddfe352f8aa11f7c35
nilichen <lichenni1990@gmail.com>
2017-04-17 23:29:20 -0700

    use local storage => login; deploy the app

--- course4-MobileDev/ionic/conFusion/www/js/services.js
+++ course4-MobileDev/ionic/conFusion/www/js/services.js
@@ -1,5 +1,22 @@
 angular.module('conFusion.services', ['ngResource'])
-.constant("baseURL","http://localhost:3000/")
+.constant("baseURL","http://192.168.0.102:3000/")
+.factory('$localStorage', ['$window', function($window) {
+  return {
+    store: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    storeObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key,defaultValue) {
+      return JSON.parse($window.localStorage[key] || defaultValue);
+    }
+  }
+}])
+
 .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
             return $resource(baseURL + "dishes/:id", null, {
