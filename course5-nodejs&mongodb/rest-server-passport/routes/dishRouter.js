1a9373cfd6ab1b994c1034ca98f460f932b6641a
nilichen <lichenni1990@gmail.com>
2017-04-24 11:42:08 -0700

    Mongoose Population

--- course5-nodejs&mongodb/rest-server-passport/routes/dishRouter.js
+++ course5-nodejs&mongodb/rest-server-passport/routes/dishRouter.js
@@ -10,7 +10,9 @@ dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
 .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-    Dishes.find({}, function (err, dish) {
+    Dishes.find({})
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
         if (err) throw err;
         res.json(dish);
     });
@@ -21,10 +23,10 @@ dishRouter.route('/')
         if (err) throw err;
         console.log('Dish created!');
         var id = dish._id;
-
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
+
         res.end('Added the dish with id: ' + id);
     });
 })
@@ -38,7 +40,9 @@ dishRouter.route('/')
 
 dishRouter.route('/:dishId')
 .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-    Dishes.findById(req.params.dishId, function (err, dish) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
         if (err) throw err;
         res.json(dish);
     });
@@ -56,14 +60,19 @@ dishRouter.route('/:dishId')
 })
 
 .delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-    Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {        if (err) throw err;
+        Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {
+        if (err) throw err;
         res.json(resp);
     });
 });
 
 dishRouter.route('/:dishId/comments')
+.all(Verify.verifyOrdinaryUser)
+
 .get(function (req, res, next) {
-    Dishes.findById(req.params.dishId, function (err, dish) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
         if (err) throw err;
         res.json(dish.comments);
     });
@@ -72,6 +81,7 @@ dishRouter.route('/:dishId/comments')
 .post(function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
+        req.body.postedBy = req.decoded._doc._id;
         dish.comments.push(req.body);
         dish.save(function (err, dish) {
             if (err) throw err;
@@ -81,7 +91,7 @@ dishRouter.route('/:dishId/comments')
     });
 })
 
-.delete(function (req, res, next) {
+.delete(Verify.verifyAdmin, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         for (var i = (dish.comments.length - 1); i >= 0; i--) {
@@ -98,8 +108,12 @@ dishRouter.route('/:dishId/comments')
 });
 
 dishRouter.route('/:dishId/comments/:commentId')
+.all(Verify.verifyOrdinaryUser)
+
 .get(function (req, res, next) {
-    Dishes.findById(req.params.dishId, function (err, dish) {
+    Dishes.findById(req.params.dishId)
+        .populate('comments.postedBy')
+        .exec(function (err, dish) {
         if (err) throw err;
         res.json(dish.comments.id(req.params.commentId));
     });
@@ -111,6 +125,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         dish.comments.id(req.params.commentId).remove();
+                req.body.postedBy = req.decoded._doc._id;
         dish.comments.push(req.body);
         dish.save(function (err, dish) {
             if (err) throw err;
@@ -122,6 +137,12 @@ dishRouter.route('/:dishId/comments/:commentId')
 
 .delete(function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
+        if (dish.comments.id(req.params.commentId).postedBy
+           != req.decoded._doc._id) {
+            var err = new Error('You are not authorized to perform this operation!');
+            err.status = 403;
+            return next(err);
+        }
         dish.comments.id(req.params.commentId).remove();
         dish.save(function (err, resp) {
             if (err) throw err;
